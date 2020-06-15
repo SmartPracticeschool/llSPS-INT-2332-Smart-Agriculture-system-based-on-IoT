55231a773efd8d839c1ab350e3dcaed17659afc2
pradeep1022 <65327197+pradeep1022@users.noreply.github.com>
2020-06-15 13:37:12 +0530

    Add files via upload

---
+++ smartinternpycode.py
@@ -0,0 +1,139 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py
+Caught exception connecting device: Unsupported authentication method: use-token-auth
+>>> 
+= RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py
+Caught exception connecting device: Unsupported authentication method: use-token-auth
+>>> 
+= RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py
+Caught exception connecting device: Unsupported authentication method: use-token-auth
+>>> 
+= RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py
+2020-06-15 11:53:34,755   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 11:54:39,977   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 11:54:39,979   ibmiotf.device.Client      CRITICAL Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+Traceback (most recent call last):
+  File "C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py", line 44, in <module>
+    deviceCli.connect()
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 230, in connect
+    self._logAndRaiseException(ConnectionException("Operation timed out connecting to IBM Watson IoT Platform: %s" % (self.address)))
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 213, in _logAndRaiseException
+    raise e
+ibmiotf.ConnectionException: Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+>>> 
+= RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py
+2020-06-15 11:57:02,224   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 11:58:07,555   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 11:58:07,563   ibmiotf.device.Client      CRITICAL Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+Traceback (most recent call last):
+  File "C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py", line 44, in <module>
+    deviceCli.connect()
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 230, in connect
+    self._logAndRaiseException(ConnectionException("Operation timed out connecting to IBM Watson IoT Platform: %s" % (self.address)))
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 213, in _logAndRaiseException
+    raise e
+ibmiotf.ConnectionException: Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+>>> 
+==================================================== RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py ===================================================
+2020-06-15 12:00:54,680   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 12:01:59,582   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 12:01:59,584   ibmiotf.device.Client      CRITICAL Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+Traceback (most recent call last):
+  File "C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py", line 44, in <module>
+    deviceCli.connect()
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 230, in connect
+    self._logAndRaiseException(ConnectionException("Operation timed out connecting to IBM Watson IoT Platform: %s" % (self.address)))
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 213, in _logAndRaiseException
+    raise e
+ibmiotf.ConnectionException: Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+
+>>> 
+==================================================== RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py ===================================================
+
+==================================================== RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py ===================================================
+2020-06-15 12:06:29,763   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 12:07:35,046   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 12:07:35,102   ibmiotf.device.Client      CRITICAL Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+Traceback (most recent call last):
+  File "C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py", line 44, in <module>
+    deviceCli.connect()
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 230, in connect
+    self._logAndRaiseException(ConnectionException("Operation timed out connecting to IBM Watson IoT Platform: %s" % (self.address)))
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 213, in _logAndRaiseException
+    raise e
+ibmiotf.ConnectionException: Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+>>> 
+==================================================== RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py ===================================================
+2020-06-15 12:09:21,375   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 12:10:26,598   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 12:10:26,615   ibmiotf.device.Client      CRITICAL Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+Traceback (most recent call last):
+  File "C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py", line 44, in <module>
+    deviceCli.connect()
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 230, in connect
+    self._logAndRaiseException(ConnectionException("Operation timed out connecting to IBM Watson IoT Platform: %s" % (self.address)))
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 213, in _logAndRaiseException
+    raise e
+ibmiotf.ConnectionException: Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+>>> 
+==================================================== RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py ===================================================
+
+====================================================================================== RESTART: Shell ======================================================================================
+>>> 
+==================================================== RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py ===================================================
+2020-06-15 12:18:05,479   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 12:19:10,745   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 12:19:10,780   ibmiotf.device.Client      CRITICAL Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+Traceback (most recent call last):
+  File "C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py", line 44, in <module>
+    deviceCli.connect()
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 230, in connect
+    self._logAndRaiseException(ConnectionException("Operation timed out connecting to IBM Watson IoT Platform: %s" % (self.address)))
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 213, in _logAndRaiseException
+    raise e
+ibmiotf.ConnectionException: Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+>>> 
+==================================================== RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py ===================================================
+2020-06-15 12:28:07,090   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 12:29:12,190   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 12:29:12,225   ibmiotf.device.Client      CRITICAL Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+Traceback (most recent call last):
+  File "C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py", line 44, in <module>
+    deviceCli.connect()
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 230, in connect
+    self._logAndRaiseException(ConnectionException("Operation timed out connecting to IBM Watson IoT Platform: %s" % (self.address)))
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 213, in _logAndRaiseException
+    raise e
+ibmiotf.ConnectionException: Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+>>> 
+==================================================== RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py ===================================================
+2020-06-15 12:30:18,283   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 12:31:23,810   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 12:31:23,835   ibmiotf.device.Client      CRITICAL Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+Traceback (most recent call last):
+  File "C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py", line 44, in <module>
+    deviceCli.connect()
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 230, in connect
+    self._logAndRaiseException(ConnectionException("Operation timed out connecting to IBM Watson IoT Platform: %s" % (self.address)))
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 213, in _logAndRaiseException
+    raise e
+ibmiotf.ConnectionException: Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+>>> 
+==================================================== RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py ===================================================
+2020-06-15 13:04:44,850   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 13:05:50,172   ibmiotf.device.Client      ERROR   Unexpected disconnect from the IBM Watson IoT Platform: 1
+2020-06-15 13:05:50,185   ibmiotf.device.Client      CRITICAL Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+Traceback (most recent call last):
+  File "C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py", line 44, in <module>
+    deviceCli.connect()
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 230, in connect
+    self._logAndRaiseException(ConnectionException("Operation timed out connecting to IBM Watson IoT Platform: %s" % (self.address)))
+  File "C:\Users\sai pradeep\AppData\Local\Programs\Python\Python38\lib\site-packages\ibmiotf\__init__.py", line 213, in _logAndRaiseException
+    raise e
+ibmiotf.ConnectionException: Operation timed out connecting to IBM Watson IoT Platform: dalm2w.messaging.internetofthings.ibmcloud.com
+>>> 
+==================================================== RESTART: C:/Users/sai pradeep/AppData/Local/Programs/Python/Python38/smartintern.py ===================================================
+2020-06-15 13:13:15,243   ibmiotf.device.Client      INFO    Connected successfully: d:dalm2w:nodebmp:1022
+Command received: {'command': 'motor turned ON'}
